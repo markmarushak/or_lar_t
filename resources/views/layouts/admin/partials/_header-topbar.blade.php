10078b519202d90d0f24507ac8f13bd3983947eb
Vitalii <serfcompany@gmail.com>
2018-04-17 17:19:15 +0300

    Add Logo Orbit leads

--- resources/views/layouts/admin/partials/_header-topbar.blade.php
+++ resources/views/layouts/admin/partials/_header-topbar.blade.php
@@ -1,8 +1,7 @@
 <div id="m_header_topbar" class="m-topbar  m-stack m-stack--ver m-stack--general">
 	<div class="m-stack__item m-topbar__nav-wrapper">
 		<ul class="m-topbar__nav m-nav m-nav--inline">
-			<li class="
-	m-nav__item m-dropdown m-dropdown--large m-dropdown--arrow m-dropdown--align-center m-dropdown--mobile-full-width m-dropdown--skin-light	m-list-search m-list-search--skin-light" m-dropdown-toggle="click" id="m_quicksearch" m-quicksearch-mode="dropdown" m-dropdown-persistent="1">
+			<li class="m-nav__item m-dropdown m-dropdown--large m-dropdown--arrow m-dropdown--align-center m-dropdown--mobile-full-width m-dropdown--skin-light	m-list-search m-list-search--skin-light" m-dropdown-toggle="click" id="m_quicksearch" m-quicksearch-mode="dropdown" m-dropdown-persistent="1">
 				<a href="#" class="m-nav__link m-dropdown__toggle">
 												<span class="m-nav__link-icon">
 													<i class="flaticon-search-1"></i>
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,13 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+
+//Affiliate Programs
+
+
+Route::get('/affiliate-service', function () {
+    return 'affiliate-service';
+})->name('home');
+
+
