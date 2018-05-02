38e88da2675492b8df8f8fc2288d191e4fc452bd
Vitalii <serfcompany@gmail.com>
2018-05-02 14:17:52 +0300

    Render Fields over qform core

--- app/Models/QformLibrary/Quform/Element/Quform_Element_Radio.php
+++ app/Models/QformLibrary/Quform/Element/Quform_Element_Radio.php
@@ -283,9 +283,9 @@ class Quform_Element_Radio extends Quform_Element_Multi implements Quform_Elemen
     public static function getDefaultConfig()
     {
         $options = array();
-        $defaults = array('Option 1' => 'quform',
-                          'Option 2' => 'quform',
-                          'Option 3' => 'quform');
+        $defaults = array(1 => 'quform',
+                          2 => 'quform',
+                          3 => 'quform');
 
 
         foreach ($defaults as $key => $value) {
