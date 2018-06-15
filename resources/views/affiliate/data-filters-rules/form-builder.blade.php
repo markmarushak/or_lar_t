dc781c6aa291574fe6d8b54b15dc22ecaf47af76
Vitaliy Kushnarev <kushnarev.vitaliy87@gmail.com>
2018-06-15 14:10:21 +0300

    new view

--- resources/views/affiliate/data-filters-rules/form-builder.blade.php
+++ resources/views/affiliate/data-filters-rules/form-builder.blade.php
@@ -16,41 +16,42 @@
 
 
         <div class="row">
+            @include('errors')
             <div class="col-xl-8">
                 <div class="qfb-dashboard qfb-cf">
                     <div class="qfb-db-row qfb-cf">
                         <div class="qfb-db-col-xl-12">
-                            @include('errors')
-                            @if(isset($forms) && !empty($forms) )
-                            <div class="qfb-box">
 
-                                <div class="qfb-cf">
 
-                                    <h3 class="qfb-box-heading qfb-db-heading">
+                            @if(isset($urls) && !empty($urls) )
+                                <div class="qfb-box">
 
-                                        <i class="mdi mdi-view_stream"></i>
-                                        Forms                        </h3>
-                                </div>
-                                <div class="qfb-content qfb-form-switcher qfb-db-form-list">
-                                    <ul class="qfb-nav-menu qfb-cf">
-                                        @foreach($forms as $form)
+                                    <div class="qfb-cf">
 
-                                        <li class="qfb-cf">
-                                           <a href="http://garasje-tilbud.no/wp-admin/admin.php?page=quform.forms&sp=edit&id=3">{{$form['name']}}<span class="qfb-fade-overflow"></span></a>
-                                            <span class="qfb-form-switcher-icons">
-                                            <a href="https://{{$dataFiltersRulesDescription}}/wp-admin/admin.php?page=quform.forms&sp=edit&id={{$form['id']}}"><i title="Edit this form" class="fa fa-pencil"></i></a>
+                                        <h3 class="qfb-box-heading qfb-db-heading">
+
+                                            <i class="mdi mdi-view_stream"></i>
+                                            Forms                        </h3>
+                                    </div>
+                                    <div class="qfb-content qfb-form-switcher qfb-db-form-list">
+                                        <ul class="qfb-nav-menu qfb-cf">
+
+                                            @foreach($urls as $namePost => $url)
+                                                <li class="qfb-cf">
+                                                    <a href="{{$url}}">{{$namePost}}<span class="qfb-fade-overflow"></span></a>
+                                                    <span class="qfb-form-switcher-icons">
+                                            <a href="{{$url}}"><i title="Edit this form" class="fa fa-pencil"></i></a>
                                         </span>
-                                        </li>
-                                        @endforeach
-                                        <li class="qfb-cf qfb-form-switcher-add-form-button qfb-form-switcher-two-buttons">
-                                            <a href="http://garasje-tilbud.no/wp-admin/admin.php?page=quform.forms&amp;sp=add">Add New</a>
-                                        </li>
+                                                </li>
+                                            @endforeach
+                                            <li class="qfb-cf qfb-form-switcher-add-form-button qfb-form-switcher-two-buttons">
+                                                <a href="http://garasje-tilbud.no/wp-admin/admin.php?page=quform.forms&amp;sp=add">Add New</a>
+                                            </li>
+                                        </ul>
 
-                                    </ul>
+                                    </div>
 
                                 </div>
-
-                            </div>
                             @endif
 
                         </div>
