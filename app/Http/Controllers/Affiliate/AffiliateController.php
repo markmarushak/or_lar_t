e6c767af3b07d4a63e477abfbd15697a76180b99
Vitaliy Kushnarev <kushnarev.vitaliy87@gmail.com>
2018-06-19 17:18:09 +0300

    new affilate partner

--- app/Http/Controllers/Affiliate/AffiliateController.php
+++ app/Http/Controllers/Affiliate/AffiliateController.php
@@ -1,78 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\Affiliate;
-
-use App\Models\SettingOfDataBase;
-use App\Plugins\QformLibrary\Quform\Element\Quform_Element_Container;
-use App\Plugins\QformLibrary\Quform\Element\Quform_Element_Field;
-use App\Plugins\QformLibrary\Quform\Element\Quform_Element_Html;
-use App\Plugins\QformLibrary\Quform\Quform_Repository;
-use App\Plugins\QformLibrary\Quform\Quform_View;
-use App\Repositories\AffiliateRepository;
-use App\Services\AffiliateService;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\DataFiltersRules;
-use App\Models\RemoteDBaccess;
-use App\Plugins\QformLibrary\Quform;
-
-//Quform Library
-use App\Plugins\QformLibrary\Quform\Quform_Form;
-
-use App\Plugins\QformLibrary\Quform\Form\Quform_Form_Factory;
-use RecursiveIteratorIterator;
-
 
 class AffiliateController extends Controller
 {
-
-
-    protected $data;
-    /**
-     * @var Quform_Repository
-     */
-    protected $repository;
-
-    /**
-     * @var Quform_Form_Factory
-     */
-    protected $formFactory;
-
-    /**
-     * @var Quform_Form_Processor
-     */
-    protected $processor;
-
-    /**
-     * @var Quform_Session
-     */
-    protected $session;
-
-    /**
-     * @var Quform_Uploader
-     */
-    protected $uploader;
-
-    /*
-     * Form counter to differentiate multiple instances of the same form
-     *
-     * @var int
-     */
-    protected $count = 0;
-
-    /**
-     * Store used unique IDs to avoid conflicts
-     *
-     * @var array
-     */
-    protected $uniqueIds = array();
-
-    protected $view;
-
-    protected $affiliateRepository;
-    protected $affiliateService;
-    protected $quformElementField;
 
     /**
      * Create a new controller instance.
--- config/database.php
+++ config/database.php
@@ -43,9 +43,9 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'orbitlea_admin'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', 'q'),
+            'database' => env('DB_DATABASE', ''),
+            'username' => env('DB_USERNAME', ''),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -54,33 +54,6 @@ return [
             'engine' => null,
         ],
 
-
-        'garage' => [
-            'driver' => 'mysql',
-            'host' => '109.199.120.183',
-            'port' => '3306',
-            'database' => 'weeklyex_wp126',
-            'username' => 'weeklyex_wp126',
-            'password' => '7d9!SO)pL4',
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-        ],
-
-
-        'garage_dev' => [
-            'driver' => 'mysql',
-            'host' => 'localhost',
-            'port' => '3306',
-            'database' => 'weeklyex_wp126',
-            'username' => 'root',
-            'password' => 'q',
-            'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
