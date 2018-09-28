eefa5f224f17c2ad2e83ddeb53fbed6ae5e3b033
Vitalii <serfcompany@gmail.com>
2018-09-28 15:45:23 +0300

    test drop33

--- app/Http/Controllers/Test/TestController.php
+++ app/Http/Controllers/Test/TestController.php
@@ -22,12 +22,10 @@ class TestController extends Controller
     public function index(Request $request = null)
     {
         $result = json_decode(json_encode(DB::table('migrations')->select('id','migration')->get()),true);
-//          DB::table('migrations')->where('id','=','15')->delete();
-//          DB::table('migrations')->where('id','=','16')->delete();
-//          DB::table('migrations')->where('id','=','17')->delete();
-//          DB::table('migrations')->where('id','=','15')->delete();
-//        Schema::drop('tab_name');
-//        Schema::drop('tab_description');
+          DB::table('migrations')->where('id','=','19')->delete();
+          DB::table('migrations')->where('id','=','20')->delete();
+        Schema::drop('tab_name');
+        Schema::drop('tab_description');
 
 
         return view('test.index',[
