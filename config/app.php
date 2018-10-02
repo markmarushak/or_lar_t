b4d05bb7a6f4bb5b9449c5e93b66acea44d1d1e0
Vitalii <serfcompany@gmail.com>
2018-10-02 16:05:36 +0300

    https

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://admin.orbitleads.com'),
+    'url' => env('APP_URL', 'https://admin.orbitleads.com'),
 
     /*
     |--------------------------------------------------------------------------
